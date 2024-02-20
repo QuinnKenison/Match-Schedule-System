95f7eb9bb32f6531aa37b151a11ff6ac105afd12
QuinnKenison <quinnkenison@gmail.com>
2024-02-20 17:00:27 -0500

    Add file upload and initialization function
    - Remove test JS feature
    - Import SheetJS script
    - Add input for uploading only .xlsx files
    - Add function to run on file upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-function test() {
-    alert("JS is working!");
+//Initialize on spreadsheet upload
+function init() {
+    var file = document.getElementById("xlsxUpload").files[0];
+    console.log("File '" + file.name + "' uploaded successfully.");
 }
