7e1c81a13fdaee4b8d36143e8d9a03d969d28175
QuinnKenison <quinnkenison@gmail.com>
2024-02-20 20:00:29 -0500

    Get values of A1, B1, C1, and print to console
    - Hide file upload on initialize
    - Create system to get value of cells A1, B1, C1 (can be copy-pasted to be used on any cell)
    
    This took forever to figure out.

--- index.js
+++ index.js
@@ -2,4 +2,19 @@
 function init() {
     var file = document.getElementById("xlsxUpload").files[0];
     console.log("File '" + file.name + "' uploaded successfully.");
+    document.getElementById("xlsxUpload").hidden = true;
+    document.getElementById("xlsxLabel").hidden = true;
+
+    var reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onload = function() {
+        var sheet = XLSX.read(reader.result, {type: "binary"});
+        console.log(sheet);
+        var cellA1 = sheet.Sheets[sheet.SheetNames[0]]["A1"].v;
+        console.log(cellA1);
+        var cellB1 = sheet.Sheets[sheet.SheetNames[0]]["B1"].v;
+        console.log(cellB1);
+        var cellC1 = sheet.Sheets[sheet.SheetNames[0]]["C1"].v;
+        console.log(cellC1);
+    };
 }
