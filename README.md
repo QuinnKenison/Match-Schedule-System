b564853d8a5d8e6e13d5bc604b7816b42f8557e2
QuinnKenison <quinnkenison@gmail.com>
2024-10-20 22:39:47 -0400

    Convert to Node.js and get file as variable
    So many things...
    - Remove old HTML/JS/CSS code
    - Add Express to create webpages using Node
    - Add Multer for handling files in Node
    - Create index.js, which starts a server (port 3000) to display webpages and takes input from webpages to get schedule PDF as a variable in memory
    - Create form.html, which is a page with a form to submit schedule PDF
    - Create app.html, which says YAY when the file is uploaded (but of course later this will be where the actual display system goes)
    - Edit README.md to better reflect new goals and execution

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Match-Schedule-System
 A match schedule display system for the pits in a VEX Robotics competition so the teams can que properly.
-- Capable of importing from Excel
+
+Uses Node.js (with Express and Multer) and HTML.
+
+- Capable of importing from schedule PDF
 - Will be screen shared using the "Vivi" application to TVs in the pits
 
 Currently live at https://quinnkenison.github.io/Match-Schedule-System/, although this is subject to change.
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-//Initialize on spreadsheet upload
-function init() {
-    var file = document.getElementById("xlsxUpload").files[0];
-    console.log("File '" + file.name + "' uploaded successfully.");
-    document.getElementById("xlsxUpload").hidden = true;
-    document.getElementById("xlsxLabel").hidden = true;
+//Setup
+const express = require('express')
+const multer  = require('multer')
+const app = express()
+const port = 3000
 
-    var reader = new FileReader();
-    reader.readAsBinaryString(file);
-    reader.onload = function() {
-        var sheet = XLSX.read(reader.result, {type: "binary"});
-        console.log(sheet);
-        var cellA1 = sheet.Sheets[sheet.SheetNames[0]]["A1"].v;
-        console.log(cellA1);
-        var cellB1 = sheet.Sheets[sheet.SheetNames[0]]["B1"].v;
-        console.log(cellB1);
-        var cellC1 = sheet.Sheets[sheet.SheetNames[0]]["C1"].v;
-        console.log(cellC1);
-    };
-}
+//Log port
+app.listen(port, function() {
+    console.log("App using port " + port)
+})
+
+//Store PDF in memory
+const upload = multer({ storage: multer.memoryStorage() });
+
+//Base page with form (/)
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/form.html")
+})
+
+//Take form input, store as a variable "schedule," and go to upload page (/upload)
+var schedule;
+app.post("/upload", upload.single("pdfUpload"), function(req, res) {
+    schedule = req.file;
+    console.log(schedule.originalname);
+    res.sendFile(__dirname + "/app.html");
+})
