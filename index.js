9f08eeca1802855b5fc99defd20fd235610a8b6a
QuinnKenison <quinnkenison@gmail.com>
2024-10-27 00:08:55 -0400

    Print entire PDF to console
    - Add pdfreader and its dependencies
    - Change upload.html to app.html for clarity
    - Use a PdfReader to get the PDF text from the stored variable
    - Print entire pdf to console

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 //Setup
 const express = require('express')
 const multer  = require('multer')
+const { PdfReader } = require("pdfreader");
+const fileSystem = require("fs");
 const app = express()
 const port = 3000
 
 //Log port
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("App using port " + port)
-})
+});
 
 //Store PDF in memory
 const upload = multer({ storage: multer.memoryStorage() });
 
 //Base page with form (/)
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/form.html")
-})
+});
 
-//Take form input, store as a variable "schedule," and go to upload page (/upload)
+//Take form input, store as a variable "schedule," and go to upload page (/app)
 var schedule;
-app.post("/upload", upload.single("pdfUpload"), function(req, res) {
+app.post("/app", upload.single("pdfUpload"), function (req, res) {
     schedule = req.file;
     console.log(schedule.originalname);
+    //Get PDF data
+    new PdfReader().parseBuffer(schedule.buffer, function (err, pdf) {
+        if (pdf && pdf.text) {
+            console.log(pdf.text);
+        }
+    });
     res.sendFile(__dirname + "/app.html");
-})
+});
